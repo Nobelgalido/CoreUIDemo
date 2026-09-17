ee0dc92636a2d39e42a34d6e2c0a5dc95d2a25c8
Nobelgalido <galidoalfred.com>
2026-09-17 13:21:54 +0800

    feat: add user service layer

---
+++ Services/IUserService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CoreUIDemo.Models;
+using CoreUIDemo.Models.ViewModels;
+
+namespace CoreUIDemo.Services
+{
+    public interface IUserService
+    {
+        List<UserListItemViewModel> GetUsers(int pageNumber, int pageSize);
+        UserListItemViewModel GetUserById(long userId);
+        void CreateUser(UserCreateViewModel model);
+        void UpdateUser(UserEditViewModel model);
+        void DeleteUser(long userId);
+
+        AuthenticatedUserData ValidateCredentials(string username, string password);
+
+        void ChangePassword(UserChangePasswordViewModel model);
+    }
+}
